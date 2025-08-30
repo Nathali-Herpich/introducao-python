366b14d81a95bb8a4eccfd063266cdba6cbed342
Nathali Herpich Valle <nathaliherpichvalle@gmail.com>
2025-08-30 12:25:13 -0300

    Add files via upload

---
+++ desafios/01.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+s = n1 * n2
+print('O resultado entre {} e {} vale {}'.format(n1,n2, s))
---
+++ desafios/02.py
@@ -0,0 +1,4 @@
+n= input('Digite um algo: ')
+print(n.isupper())
+print(n.isalpha())
+print(n.isalnum())
