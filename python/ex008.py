20838ee58109b77763bec2ff0fa1cc7869a23f98
Nathali Herpich Valle <nathaliherpichvalle@gmail.com>
2025-08-30 12:24:50 -0300

    Add files via upload

---
+++ python/ex008.py
@@ -0,0 +1,21 @@
+n = float(input('Digite um valor:'))
+print(n)
+
+n= bool(input('Digite um valor: '))
+print(n)
+
+#vai dizer se é possível converter o tipo primitivo em int
+n = input('Digite um algo ')
+print(n.isnumeric())
+
+# vai dizer se ele é do alfabeto
+n = input('Digite algo: ')
+print(n.isalpha())
+
+#vai dizer se é alpha numérico
+n = input('Digite algo: ')
+print(n.isalnum())
+
+#ele vai ver se está soemente com letras maiúscula
+n = input ('Digite algo: ')
+print(n.isupper())
